9ec22ca9bd9463fded6297131cb2f0cc6517ca57
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-11-06 23:05:15 -0300

    feat(install): Check required PHP extensions

--- include/config_default.inc.php
+++ include/config_default.inc.php
@@ -6,6 +6,42 @@
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
 
+if (! extension_loaded('curl')) {
+    exit('PHP extension "curl" is not loaded');
+}
+
+if (! extension_loaded('exif')) {
+    exit('PHP extension "exif" is not loaded');
+}
+
+if (! extension_loaded('fileinfo')) {
+    exit('PHP extension "fileinfo" is not loaded');
+}
+
+if (! extension_loaded('gd')) {
+    exit('PHP extension "gd" is not loaded');
+}
+
+if (! extension_loaded('iconv')) {
+    exit('PHP extension "iconv" is not loaded');
+}
+
+if (! extension_loaded('mbstring')) {
+    exit('PHP extension "mbstring" is not loaded');
+}
+
+if (! extension_loaded('mysqli')) {
+    exit('PHP extension "mysqli" is not loaded');
+}
+
+if (! extension_loaded('openssl')) {
+    exit('PHP extension "openssl" is not loaded');
+}
+
+if (! extension_loaded('tokenizer')) {
+    exit('PHP extension "tokenizer" is not loaded');
+}
+
 require_once PHPWG_ROOT_PATH . 'vendor/autoload.php';
 
 /**

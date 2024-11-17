dc0119d0c2c5563b25218079565e9d9acb7be1d9
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-11-17 14:37:20 -0300

    feat(install): Add MySQL socket connection option

--- install.php
+++ install.php
@@ -111,7 +111,7 @@ $dbpasswd = (empty($_POST['dbpasswd'])) ? '' : $_POST['dbpasswd'];
 $dbname = (empty($_POST['dbname'])) ? '' : $_POST['dbname'];
 
 // dblayer
-$dblayer = 'mysqli';
+$dblayer = (empty($_POST['dbtype'])) ? 'mysqli' : str_replace('-socket', '', $_POST['dbtype']);
 
 $admin_name = (empty($_POST['admin_name'])) ? '' : $_POST['admin_name'];
 $admin_pass1 = (empty($_POST['admin_pass1'])) ? '' : $_POST['admin_pass1'];

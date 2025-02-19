d201794660a6687546de80c78823accdbdf510db
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2025-02-18 22:24:52 -0300

    config(piwigo): Disable PHP error display on frontend
    Showing errors breaks scripts/pages.

--- include/config_default.inc.php
+++ include/config_default.inc.php
@@ -503,14 +503,14 @@ $conf['template_combine_files'] = true;
 // this permit to show the php errors reporting (see INI 'error_reporting'
 // for possible values)
 // gives an empty value '' to deactivate
-$conf['show_php_errors'] = E_ALL;
+$conf['show_php_errors'] = ''; // E_ALL;
 
 // This sets the display_errors php option to true, so php errors and warning
 // messages are shown in the browser. If this is false, the error messages are 
 // available in the php log of the server if show_php_errors has any set.
 // If the below is turned off in local config and errors are still shown on 
 // frontend, check for display_errors setting server's php config
-$conf['show_php_errors_on_frontend'] = true;
+$conf['show_php_errors_on_frontend'] = false; // true;
 
 
 // +-----------------------------------------------------------------------+

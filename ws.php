5ef9ac37a0c985f4e97ac48956692d5d2a124620
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2025-03-30 18:30:44 -0300

    refactor(php): Remove calls to non-existent functions

--- ws.php
+++ ws.php
@@ -95,15 +95,6 @@ function ws_addDefaultMethods( $arr )
   );
 
   $service->addMethod(
-    'pwg.activity.downloadLog',
-    ws_activity_downloadLog(...),
-    null,
-    'Returns general informations.',
-    $ws_functions_root . 'pwg.php',
-    array('admin_only'=>true)
-  );
-
-  $service->addMethod(
       'pwg.caddie.add',
       ws_caddie_add(...),
       array(

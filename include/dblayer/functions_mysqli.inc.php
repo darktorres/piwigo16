164449d364188bb2e5987d78a2d179d0d552ce1b
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2025-04-09 20:18:38 -0300

    docs(install): Update MySQL/PHP minimum versions

--- include/dblayer/functions_mysqli.inc.php
+++ include/dblayer/functions_mysqli.inc.php
@@ -11,7 +11,7 @@
  */
 
 define('DB_ENGINE', 'MySQL');
-define('REQUIRED_MYSQL_VERSION', '5.0.0');
+define('REQUIRED_MYSQL_VERSION', '8.4.4');
 
 define('DB_REGEX_OPERATOR', 'REGEXP');
 define('DB_RANDOM_FUNCTION', 'RAND');

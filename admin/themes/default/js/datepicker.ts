caaa40ed99cbe847e9131bbd29c126f53e8f0068
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2026-04-29 20:39:07 -0300

    refactor(js): replace jQuery UI datepicker+timepicker with Flatpickr; expose pwgDatepicker on window

--- admin/themes/default/js/datepicker.ts
+++ admin/themes/default/js/datepicker.ts
@@ -1,135 +1,88 @@
-(function ($: JQueryStatic) {
-    (jQuery as any).timepicker.log = jQuery.noop;
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.css';
 
-    const origGenerateMonthYearHeader = ($.datepicker as any)._generateMonthYearHeader;
-    const origSelectMonthYear = ($.datepicker as any)._selectMonthYear;
+interface PwgDatepickerOptions {
+    showTimepicker?: boolean;
+    cancelButton?: string | false;
+    [key: string]: unknown;
+}
 
-    ($.datepicker as any)._generateMonthYearHeader = function (
-        this: unknown, inst: unknown, drawMonth: unknown, drawYear: unknown,
-        minDate: unknown, maxDate: unknown, secondary: unknown, monthNames: unknown, monthNamesShort: unknown
-    ) {
-        const html: string = origGenerateMonthYearHeader.call(this, inst, drawMonth, drawYear, minDate, maxDate, secondary, monthNames, monthNamesShort);
-        const yearshtml = `<input type='number' class='ui-datepicker-year' data-handler='selectYear' data-event='change keyup' value='${drawYear}' style='width:4em;margin-left:2px;'>`;
-        return html.replace(new RegExp("<select class='ui-datepicker-year'.*</select>", 'gm'), yearshtml);
+function pwgDatepicker(el: HTMLElement, settings: PwgDatepickerOptions = {}): void {
+    const options = { showTimepicker: false, cancelButton: false, ...settings };
+    const targetName = (el as HTMLElement).dataset['datepicker'];
+    const targetEl = targetName ? document.querySelector<HTMLInputElement>(`[name="${targetName}"]`) : null;
+    const linked = !!targetEl;
+
+    let originalValue = String(linked ? (targetEl!.value ?? '') : (el as HTMLInputElement).value ?? '');
+
+    const fpOptions: flatpickr.Options.Options = {
+        enableTime: !!options.showTimepicker,
+        dateFormat: linked ? 'l, j F Y' + (options.showTimepicker ? ' H:i' : '') : 'Y-m-d' + (options.showTimepicker ? ' H:i' : ''),
+        altInput: linked,
+        altFormat: 'Y-m-d' + (options.showTimepicker ? ' H:i:S' : ''),
+        altInputClass: linked ? (el as HTMLInputElement).className : '',
+        allowInput: true,
+        onChange: linked ? [(selectedDates, dateStr) => {
+            if (targetEl) targetEl.value = dateStr ? dateStr.split(' ')[0] : '';
+        }] : [],
     };
 
-    ($.datepicker as any)._selectMonthYear = debounce(function (this: unknown, ...args: unknown[]) {
-        const id = args[0] as string;
-        const select = args[1] as HTMLInputElement;
-        const period = args[2] as string;
-        if (period === 'M') {
-            origSelectMonthYear.call(this, id, select, period);
-        } else {
-            const target = $(id);
-            const inst = ($.datepicker as any)._getInst(target[0]);
-            const val = parseInt(select.value, 10);
-            if (isNaN(val)) {
-                inst['drawYear'] = '';
-            } else {
-                inst['selectedYear'] = inst['drawYear'] = val;
-                ($.datepicker as any)._notifyChange(inst);
-                ($.datepicker as any)._adjustDate(target);
-                $('.ui-datepicker-year').trigger('focus');
-            }
+    // Link start/end date pickers
+    const startName = (el as HTMLElement).dataset['datepickerStart'];
+    const endName = (el as HTMLElement).dataset['datepickerEnd'];
+
+    if (startName) {
+        const startEl = document.querySelector<HTMLElement>(`[data-datepicker="${startName}"]`);
+        if (startEl) {
+            const startFp = (startEl as any)._flatpickr as flatpickr.Instance | undefined;
+            if (startFp) fpOptions.minDate = startFp.selectedDates[0] ?? undefined;
+            fpOptions.onClose = [(_dates, dateStr) => {
+                if (startFp) startFp.set('maxDate', dateStr || undefined);
+            }];
         }
-    }, 500);
-
-    (jQuery.fn as any).pwgDatepicker = function (settings: Record<string, unknown>) {
-        const options = jQuery.extend(true, { showTimepicker: false, cancelButton: false }, settings || {});
-
-        return this.each(function (this: HTMLElement) {
-            const $this = jQuery(this);
-            let originalValue = String($this.val() ?? '');
-            let originalDate: Date | null;
-            const $target = jQuery('[name="' + $this.data('datepicker') + '"]');
-            const linked = !!$target.length;
-            let $start: JQuery, $end: JQuery;
-
-            if (linked) originalValue = String($target.val() ?? '');
-
-            function set(date: Date | null | string, init: boolean) {
-                if (date === '') date = null;
-                ($this as any).datetimepicker('setDate', date);
-
-                if ($this.data('datepicker-start') && $start) {
-                    ($start as any).datetimepicker('option', 'maxDate', date);
-                } else if ($this.data('datepicker-end') && $end && !init) {
-                    ($end as any).datetimepicker('option', 'minDate', date);
-                }
-                if (!date && linked) $target.val('');
-            }
-
-            if (options.cancelButton) {
-                options.beforeShow = options.onChangeMonthYear = function () {
-                    setTimeout(function () {
-                        const buttonPane = ($this as any).datepicker('widget').find('.ui-datepicker-buttonpane');
-                        if (buttonPane.find('.pwg-datepicker-cancel').length === 0) {
-                            $('<button type="button">' + options.cancelButton + '</button>')
-                                .on('click', function () { set(originalDate, false); ($this as any).datepicker('hide').blur(); })
-                                .addClass('pwg-datepicker-cancel ui-state-error ui-corner-all')
-                                .appendTo(buttonPane);
-                        }
-                    }, 1);
-                };
-            }
-
-            ($this as any).datetimepicker(jQuery.extend({
-                dateFormat: linked ? 'DD d MM yy' : 'yy-mm-dd',
-                timeFormat: 'HH:mm',
-                separator: options.showTimepicker ? ' ' : '',
-                altField: linked ? $target : null,
-                altFormat: 'yy-mm-dd',
-                altTimeFormat: options.showTimepicker ? 'HH:mm:ss' : '',
-                autoSize: true,
-                changeMonth: true,
-                changeYear: true,
-                altFieldTimeOnly: false,
-                showSecond: false,
-                alwaysSetTime: false,
-            }, options));
-
-            if ($this.data('datepicker-start')) {
-                $start = jQuery('[data-datepicker="' + $this.data('datepicker-start') + '"]');
-                ($this as any).datetimepicker('option', 'onClose', (date: Date) => { ($start as any).datetimepicker('option', 'maxDate', date); });
-                ($this as any).datetimepicker('option', 'minDate', ($start as any).datetimepicker('getDate'));
-            } else if ($this.data('datepicker-end')) {
-                $end = jQuery('[data-datepicker="' + $this.data('datepicker-end') + '"]');
-                ($this as any).datetimepicker('option', 'onClose', (date: Date) => { ($end as any).datetimepicker('option', 'minDate', date); });
-            }
-
-            if ($this.data('datepicker-unset')) {
-                jQuery('#' + $this.data('datepicker-unset')).on('click', (e) => { e.preventDefault(); set(null, false); });
-            }
-
-            if (linked) {
-                const splitted = originalValue.split(' ');
-                if (splitted.length === 2 && options.showTimepicker) {
-                    set((jQuery.datepicker as any).parseDateTime('yy-mm-dd', 'HH:mm:ss', originalValue), true);
-                } else if (splitted[0].length === 10) {
-                    set(jQuery.datepicker.parseDate('yy-mm-dd', splitted[0]), true);
-                } else {
-                    set(null, true);
-                }
-            }
-
-            originalDate = ($this as any).datetimepicker('getDate') as Date;
-            if (options.showTimepicker) {
-                $this.attr('size', String(parseInt(String($this.attr('size') ?? '0')) + 6));
-            }
-        });
-    };
-
-    function debounce(func: (...args: unknown[]) => unknown, wait: number, immediate?: boolean) {
-        let timeout: ReturnType<typeof setTimeout> | null;
-        return function (this: unknown, ...args: unknown[]) {
-            const later = () => {
-                timeout = null;
-                if (!immediate) func.apply(this, args);
-            };
-            const callNow = immediate && !timeout;
-            if (timeout) clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) func.apply(this, args);
-        };
+    } else if (endName) {
+        const endEl = document.querySelector<HTMLElement>(`[data-datepicker="${endName}"]`);
+        if (endEl) {
+            const endFp = (endEl as any)._flatpickr as flatpickr.Instance | undefined;
+            fpOptions.onClose = [(_dates, dateStr) => {
+                if (endFp) endFp.set('minDate', dateStr || undefined);
+            }];
+        }
     }
-}(jQuery));
+
+    const fp = flatpickr(el, fpOptions);
+    (el as any)._flatpickr = fp;
+
+    // Set initial value
+    if (linked && originalValue) {
+        const parts = originalValue.split(' ');
+        if (parts[0].length === 10) {
+            fp.setDate(originalValue, false);
+        }
+    }
+
+    // Cancel button (unset button)
+    const unsetId = (el as HTMLElement).dataset['datepickerUnset'];
+    if (unsetId) {
+        document.getElementById(unsetId)?.addEventListener('click', (e) => {
+            e.preventDefault();
+            fp.clear();
+            if (linked && targetEl) targetEl.value = '';
+        });
+    }
+
+    if (options.showTimepicker) {
+        const sizeAttr = (el as HTMLInputElement).size;
+        if (sizeAttr) (el as HTMLInputElement).size = sizeAttr + 6;
+    }
+}
+
+(window as any).pwgDatepicker = pwgDatepicker;
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll<HTMLElement>('[data-datepicker]').forEach(el => {
+        pwgDatepicker(el);
+    });
+});
+
+export {};

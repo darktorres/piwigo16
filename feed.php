1efe1563912ad0aed6289b59cd1f569a96828dd9
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2026-05-01 14:19:43 -0300

    chore(feedcreator): replace vendored feedcreator.class.php with openpsa/universalfeedcreator package

--- feed.php
+++ feed.php
@@ -85,8 +85,6 @@ check_status(ACCESS_GUEST);
 list($dbnow) = pwg_db_fetch_row(pwg_query('SELECT NOW();')) ?? [null];
 $dbnow = $dbnow ? (string)$dbnow : null;
 
-include_once(PHPWG_ROOT_PATH.'include/feedcreator.class.php');
-
 set_make_full_url();
 
 $rss = new UniversalFeedCreator();

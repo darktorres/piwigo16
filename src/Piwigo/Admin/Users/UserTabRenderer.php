ed00c762e9981e4af7dce84b2fb30203c001c01a
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2026-05-06 11:14:28 -0300

    fix: set GLOBALS[my_base_url] in UserTabRenderer before firing users tabsheet event

--- src/Piwigo/Admin/Users/UserTabRenderer.php
+++ src/Piwigo/Admin/Users/UserTabRenderer.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Piwigo\Admin\Users;
 
 use Piwigo\Admin\Tabsheet;
+use Piwigo\Core\ServiceLocator;
+use Piwigo\Url\UrlGenerator;
 
 final class UserTabRenderer
 {
@@ -12,6 +14,7 @@ final class UserTabRenderer
     {
         /** @var array<string, mixed> $page */
         $page = is_array($GLOBALS['page'] ?? null) ? $GLOBALS['page'] : [];
+        $GLOBALS['my_base_url'] = ServiceLocator::get(UrlGenerator::class)->admin() . '&page=';
         $tabsheet = new Tabsheet();
         $tabsheet->set_id('users');
         $tabsheet->select(is_string($page['tab'] ?? null) ? $page['tab'] : '');
